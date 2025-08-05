a8e5761173a828135666b0a3149ce7099d6cdb98
shantanu khise <shantanukhise2004@gmail.com>
2025-08-05 23:23:08 +0530

    Add files via upload

---
+++ C module/assignment 3/table.c
@@ -0,0 +1,14 @@
+//Print table for given number.
+//Input: n = 5
+#include<stdio.h>
+void main(){
+	int no=5,i=1;
+	printf("Table of 5:\n");
+		while(i<=10){
+		printf("5 * %d = %d\n",i,5*i);
+		i++;
+		
+	}
+	
+	
+}

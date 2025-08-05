2cfa51f80b64c38ccab2744fcc3c88c9f5c2a1fa
shantanu khise <shantanukhise2004@gmail.com>
2025-08-05 23:41:34 +0530

    Add files via upload

---
+++ C module/assignment 4/findperfectnoinrange.c
@@ -0,0 +1,23 @@
+//Print perfect numbers in the given range 1 to n.
+#include<stdio.h>
+void main() {
+	int no;
+	printf("enter the range:");
+	scanf("%d,",&no);
+	for(int k=1;k<=no;k++){
+	
+	int num=k,i=1,sum=0;
+	
+	while(i<=num/2){
+		
+		if(num%i==0){
+			sum=sum+i;
+		}i++;
+		
+	}
+	
+	if(sum==num){
+		printf("%d\n",k);
+	}
+}
+}

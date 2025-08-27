f331f20a4b9e4705bec96ecd792dfedeaa8036af
shantanu khise <shantanukhise2004@gmail.com>
2025-08-27 23:07:26 +0530

    Add files via upload

---
+++ C module/Test's/Test 1/last2digit.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+void main(){
+	int no,lasttwodigit;
+	printf("Enter the Number:");
+	scanf("%d",&no);
+	lasttwodigit=no%100;
+	printf("Last two digit :%d",lasttwodigit);
+	
+	
+}

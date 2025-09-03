6cab5e12f9d58c10791895b8790b38f1a56ff71a
shantanu khise <shantanukhise2004@gmail.com>
2025-09-03 22:06:15 +0530

    Add files via upload

---
+++ C module/assignment 12/removeoddindex.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+void main(){
+	char str[]="shantanu";
+	int i=0,j=0;
+	while(str[i]!='\0'){
+		if(i%2==0){
+			str[j++]=str[i];
+//			j++;
+		}
+		i++;
+		
+}
+	str[j]='\0';
+	
+	printf("%s",str);
+}
